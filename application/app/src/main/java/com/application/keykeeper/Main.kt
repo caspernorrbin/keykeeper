eb2f8d2f7d6f286bddce7337f92178ba293dd4fc
JoXW100 <joxw100@gmail.com>
2023-02-07 14:41:11 +0100

    Contrained fragments between toolbar and navigation menu

--- application/app/src/main/java/com/application/keykeeper/Main.kt
+++ application/app/src/main/java/com/application/keykeeper/Main.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -19,10 +20,10 @@ class Main : AppCompatActivity() {
         setContentView(R.layout.main)
         setSupportActionBar(findViewById(R.id.toolbar))
         var config = AppBarConfiguration(setOf(R.id.nav_debug_fragment, R.id.nav_storage_fragment, R.id.nav_account_fragment))
-        navController = findNavController(R.id.nav_host_fragment)
+        var navHost = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         bottomNav = findViewById(R.id.bottom_navigation_view)
-        bottomNav.setupWithNavController(navController)
-        setupActionBarWithNavController(navController, config)
+        bottomNav.setupWithNavController(navHost.navController)
+        setupActionBarWithNavController(navHost.navController, config)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
