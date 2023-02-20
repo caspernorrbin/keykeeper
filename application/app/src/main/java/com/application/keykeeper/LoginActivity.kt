c5e7871d43640c948cf5850343168c2677378a90
Jakob Wallén <57936645+JoXW100@users.noreply.github.com>
2023-02-20 13:51:35 +0100

    Implemented animated loading icon and integrated it with the login view (#59)

--- application/app/src/main/java/com/application/keykeeper/LoginActivity.kt
+++ application/app/src/main/java/com/application/keykeeper/LoginActivity.kt
@@ -1,23 +1,26 @@
 package com.application.keykeeper
 
+import android.content.Context
 import android.content.Intent
-import android.graphics.Color
+import android.graphics.drawable.Animatable
 import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import android.view.inputmethod.InputMethodManager
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var bodyLayout: LinearLayout
     private lateinit var emailInput: EditText
     private lateinit var passwordInput: EditText
     private lateinit var buttonLogin: Button
     private lateinit var statusMessage: TextView
     private lateinit var buttonCreateAccount: Button
+    private lateinit var loadingIcon: ImageView
+
 
     private fun showStatusMessage(message: String, isErrorMessage: Boolean = false) {
         statusMessage.text = message
@@ -29,7 +32,6 @@ class LoginActivity : AppCompatActivity() {
         }
 
         statusMessage.visibility = View.VISIBLE
-
     }
 
     private fun hideStatusMessage() {
@@ -41,11 +43,15 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         // Find elements
+        bodyLayout = findViewById(R.id.login_body_layout)
         emailInput = findViewById(R.id.login_email_input)
         passwordInput = findViewById(R.id.login_password_input)
         buttonLogin = findViewById(R.id.login_button);
         statusMessage = findViewById(R.id.login_status_message)
         buttonCreateAccount = findViewById(R.id.login_create_button)
+        loadingIcon = findViewById(R.id.login_loading_icon)
+
+        swapBodyLoading(false)
 
         // Add onClick listeners.
         buttonLogin.setOnClickListener {
@@ -55,13 +61,14 @@ class LoginActivity : AppCompatActivity() {
             // Check if valid email and non-empty password
             if(email.isValidEmail() && password.isNotEmpty()) {
                 val ac = Account(email, password)
-
+                swapBodyLoading(true)
                 ac.sendLoginRequest { successful, message ->
                     if(successful) {
                         hideStatusMessage()
                         navigateToMain()
                     } else {
                         // Display error message that input is invalid.
+                        swapBodyLoading(false)
                         showStatusMessage(message, true)
                     }
                 }
@@ -95,6 +102,36 @@ class LoginActivity : AppCompatActivity() {
 
         val intent = Intent(this@LoginActivity, CreateAccountActivity::class.java)
         startActivity(intent)
+    }
+
+    private fun swapBodyLoading(loading: Boolean) {
+        if (loading) {
+            // Hide keyboard, must be done before input is invisible
+            currentFocus?.let {
+                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+                imm?.hideSoftInputFromWindow(it.windowToken, 0)
+            }
+
+            hideStatusMessage()
+
+            bodyLayout.visibility = View.INVISIBLE
+            loadingIcon.visibility = View.VISIBLE
+            // Start animating loading icon
+            (loadingIcon.drawable as Animatable).start()
+
+            // Necessary to disable buttons to hide keyboard if they are selected
+            buttonLogin.isEnabled = false
+            buttonCreateAccount.isEnabled = false
+
+        } else {
+            // Stop animating loading icon
+            (loadingIcon.drawable as Animatable).stop()
+            bodyLayout.visibility = View.VISIBLE
+            loadingIcon.visibility = View.GONE
+
+            buttonLogin.isEnabled = true
+            buttonCreateAccount.isEnabled = true
+        }
     }
 
     private fun String.isValidEmail(): Boolean {
