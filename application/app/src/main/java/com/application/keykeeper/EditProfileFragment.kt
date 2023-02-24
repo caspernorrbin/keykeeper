4192c187d69470c1d6c962950c3b360b72cfb4ae
Amer Slaiman <amer.m.sl.248@gmail.com>
2023-02-24 16:27:50 +0100

    changing in button
    -remove the check boxes.
    -disable the "Save Changes" button initially and adds a text change listener to the old password field. The text change listener enables the button only when the old password field is not empty.

--- application/app/src/main/java/com/application/keykeeper/EditProfileFragment.kt
+++ application/app/src/main/java/com/application/keykeeper/EditProfileFragment.kt
@@ -1,21 +1,20 @@
 package com.application.keykeeper
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.CheckBox
 import android.widget.EditText
 import structure.User
 
 
 class EditProfileFragment : Fragment() {
     private lateinit var viewOfLayout: View
-    private lateinit var checkBoxEmail: CheckBox
     private lateinit var editTextEmail: EditText
-    private lateinit var checkBoxPassword: CheckBox
     private lateinit var editTextPassword: EditText
     private lateinit var editTextOldPassword: EditText
     private lateinit var buttonSaveChanges: Button
@@ -28,27 +27,31 @@ class EditProfileFragment : Fragment() {
         viewOfLayout = inflater.inflate(R.layout.fragment_edit_profile, container, false)
 
         // Initialize views
-        checkBoxEmail = viewOfLayout.findViewById(R.id.checkBoxEmail)
         editTextEmail = viewOfLayout.findViewById(R.id.editTextEmail)
-        checkBoxPassword = viewOfLayout.findViewById(R.id.checkBoxPassword)
         editTextPassword = viewOfLayout.findViewById(R.id.editTextPassword)
         editTextOldPassword = viewOfLayout.findViewById(R.id.editTextOldPassword)
         buttonSaveChanges = viewOfLayout.findViewById(R.id.buttonSaveChanges)
+
+        // Disable the "Save Changes" button initially
+        buttonSaveChanges.isEnabled = false
+
+        // Add a text change listener to the old password field
+        editTextOldPassword.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                // Enable the button if the old password field is not empty
+                buttonSaveChanges.isEnabled = !s.isNullOrEmpty()
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
 
 
         // Set click listener for "Save Changes" button
         buttonSaveChanges.setOnClickListener {
             saveChanges()
         }
-        // Set enabled state of email and password fields based on checkbox state
-        checkBoxEmail.setOnCheckedChangeListener { _, isChecked ->
-            editTextEmail.isEnabled = isChecked
-        }
-
-        checkBoxPassword.setOnCheckedChangeListener { _, isChecked ->
-            editTextPassword.isEnabled = isChecked
-        }
-
         // Fetch user data from the database
         user = getUserInfo()
 
@@ -60,9 +63,6 @@ class EditProfileFragment : Fragment() {
     }
     private fun saveChanges() {
 
-        // Check which fields the user wants to update
-        val updateEmail = checkBoxEmail.isChecked
-        val updatePassword = checkBoxPassword.isChecked
         val oldPassword = editTextOldPassword.text.toString()
 
 
@@ -72,15 +72,15 @@ class EditProfileFragment : Fragment() {
             return
         }
 
-        // Update the corresponding fields if the user has selected them
-        if (updateEmail) {
-            val newEmail = editTextEmail.text.toString()
+        val newEmail = editTextEmail.text.toString()
+        val newPassword = editTextPassword.text.toString()
+
+        // Update the user data if necessary
+        if (newEmail != user.email) {
             user.email = newEmail
         }
 
-
-        if (updatePassword) {
-            val newPassword = editTextPassword.text.toString()
+        if (newPassword != user.password) {
             user.password = newPassword
         }
 
