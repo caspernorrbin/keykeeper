1b84b944dac1e21acad213ca5feb6010200719ee
Casper Norrbin <casper.norrbin.1150@student.uu.se>
2023-03-02 00:48:31 +0100

    Update account endpoint

--- server/src/routes/account.js
+++ server/src/routes/account.js
@@ -40,6 +40,68 @@ router.post('/create', async (req, res) => {
     });
 });
 
+router.post("/update", async (req, res) => {
+    if (!req.session.loggedIn) {
+        return res.status(403).send();
+    }
+    const id = req.session.accountId;
+
+    const { oldpassword, email, password, symkey } = req.body;
+
+    // Old password and new password is required
+    if (!oldpassword || !password) {
+        return res.status(400).json({ message: "Empty old password or new password field" });
+    }
+
+    if (email) {
+        // Validate email format
+        const emailRegex = /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/ig;
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({ message: "Invalid email format" });
+        }
+
+        // Check if email is already in use
+        const account = Account.get(email);
+        if (account && account.id != id) {
+            return res.status(400).json({ message: "Email already in use" });
+        }
+    }
+
+    // Check if account exists
+    const account = Account.getById(id);
+    if (!account) {
+        return res.status(400).json({ message: "Account not updated" });
+    }
+
+    // Check if old password is correct
+    bcrypt.compare(oldpassword, account.password_hash, (err, result) => {
+        if (err) {
+            console.log(err);
+            return res.status(400).json({ message: "Account not updated" });
+        }
+        if (result) {
+            // Hash new password
+            saltRounds = 10;
+            bcrypt.hash(password, saltRounds, (err, hash) => {
+                if (err) {
+                    console.log(err);
+                    return res.status(400).json({ message: "Account not updated" });
+                }
+
+                // Update account
+                const success = Account.update(id, email, hash, symkey);
+                if (success) {
+                    return res.status(200).json({ message: "Account updated" });
+                }
+                return res.status(400).json({ message: "Account not updated" });
+            });
+        }
+        else {
+            return res.status(400).json({ message: "Password incorrect" });
+        }
+    });
+});
+    
 // delete account request
 router.post("/delete", async (req, res) => {
     const { email, password } = req.body;
